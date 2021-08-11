37d82b9ca9ecf0c4e8612a40baa29a63eede6c79
Sergei Zait <sergeizster@gmail.com>
2021-08-11 14:24:21 +0300

    add wall 2

--- src/com/sergei/Test.java
+++ src/com/sergei/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hello sergei");
-		System.out.println("world wall");
+		System.out.println("world wall2");
 		
 		System.out.println(" add another 4");
 
